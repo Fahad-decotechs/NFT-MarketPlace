4da3e68e65e7e3e380d0064fdf5f62f140fa9864
hamzashamim182001 <syedhamza182001@gmail.com>
2024-03-26 14:43:48 +0500

    Commiting Next Application

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -2,17 +2,16 @@
 
 // 1. Set up the network your smart contracts are deployed to.
 // First, import the chain from the package, then set the NETWORK variable to the chain.
-import { Mumbai } from "@thirdweb-dev/chains";
-export const NETWORK = Mumbai;
+import { Sepolia } from "@thirdweb-dev/chains";
+export const NETWORK = Sepolia;
 
 // 2. The address of the marketplace V3 smart contract.
 // Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
-export const MARKETPLACE_ADDRESS = "0x83c57ec0dF015eef8401fFb7CB7f66CfA8b6Ff55";
+export const MARKETPLACE_ADDRESS = "0x341A13B3668bF6CF5C19978689c8B2f2B9080C9D";
 
 // 3. The address of your NFT collection smart contract.
-export const NFT_COLLECTION_ADDRESS =
-  "0x67b40fC017863743C520cDAec1B367BE1FD721f0";
+export const NFT_COLLECTION_ADDRESS = "0x57EE98dA09F0E2fE482BE6B903D6C22f184573F3";
 
 // (Optional) Set up the URL of where users can view transactions on
 // For example, below, we use Mumbai.polygonscan to view transactions on the Mumbai testnet.
-export const ETHERSCAN_URL = "https://mumbai.polygonscan.com";
+export const ETHERSCAN_URL = "https://sepolia.etherscan.io";
